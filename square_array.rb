9b3bc6204f14a2119b265224e82b9d842221d54b
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-12-17 23:47:17 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_numbers = [1,2,3]
+ numbers.each do |num|
+   new_numbers << (9,10,16,25)
+
 end
+
+return new_numbers
+end 

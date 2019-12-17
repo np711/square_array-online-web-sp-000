6d49faa750b4c0e084ed305a49b3ab6d31606cf0
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-12-17 23:46:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
-  new_numbers = []
+def square_array(numbers)
+  new_numbers = [1,2,3]
  numbers.each do |num|
-   new_numbers << (num ** 2)
+   new_numbers << (9,10,16,25)
 
 end
 
